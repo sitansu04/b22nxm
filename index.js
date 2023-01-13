ebf5c0ebf3e20752bc9e9e9af0d5e8f74b777eae
Sitansu Mandal <85841972+sitansu04@users.noreply.github.com>
2023-01-13 21:55:10 +0530

    ii

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { connection } = require("./config/db.js");
+const { userRouter } = require("./routes/user.routes.js");
+const { noteRouter } = require("./routes/Note.routes.js");
+const { authenticate } = require("./middlewares/authenticate.middleware.js");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.port;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRouter);
+app.use(authenticate);
+app.use("/notes", noteRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log("Connection Established");
+  } catch (error) {
+    console.log("Err: Connection in DB");
+    console.log(error);
+  }
+  console.log(`listening on port ${port}`);
+});
+
+// {
+//   "title":"Frontend",
+//   "note":"Crud PSC",
+//   "catagory":"Live Session",
+//   "author":"Sitansu"
+// }
